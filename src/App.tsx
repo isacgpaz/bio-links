3fda834253daa4379c72f2d2a982f66587f6a0a1
Isac Gomes <isacgomesp@gmail.com>
2023-02-22 20:29:13 -0300

    feat: add header description

--- src/App.tsx
+++ src/App.tsx
@@ -3,50 +3,58 @@ import { LinkProps } from "./ts/link";
 
 const links: LinkProps[] = [
   {
+    title: "WhatsApp",
+    url: "https://api.whatsapp.com/send?phone=5588981294728",
+    description: "Quer falar comigo de forma rápida?",
+  },
+  {
     title: "Github",
     url: "https://github.com/isacgpaz",
-    description: "Some projects I developed",
+    description: "Alguns projetos que desenvolvi",
   },
   {
     title: "Linkedin",
     url: "https://linkedin.com/in/isacgpaz",
-    description: "Legal publications about technology",
+    description: "Alguns posts legais sobre tecnologia",
   },
   {
-    title: "Email",
+    title: "E-mail",
     url: "mailto:isacgomesp@gmail.com",
-    description: "Contact me",
+    description: "Entre em contato comigo",
   },
 ];
 
+// <p>🤓 Amo estudar e desenvolver coisas</p>
+//             <p>🎯 Foco na evolução</p>
+
 export function App() {
   return (
-    <div className="bg-gradient-to-t from-gray-900 to-emerald-900 min-h-screen min-w-full">
+    <div className="bg-gradient-to-t from-gray-900 to-emerald-900 min-h-screen min-w-full pb-16">
       <div className="max-w-md mx-auto flex flex-col items-center">
         <header className="pt-12 text-center text-white">
           <img
             src="https://github.com/isacgpaz.png"
             alt="Foto de Isac Paz"
-            className="rounded-full border-emerald-600 border-2 w-28 h-28"
+            className="rounded-full border-emerald-600 border-4 w-28 h-28"
           />
 
-          <span className="block mt-3 text-lg font-medium">Isac Paz</span>
+          <span className="block mt-3 text-2xl font-medium">Isac Paz</span>
 
-          <span className="block text-xs opacity-60">@isacgpaz</span>
+          <span className="block text-sm opacity-60">@isacgpaz</span>
         </header>
 
-        <main className="text-white text-center mt-6">
-          <h1 className="text-lg font-medium">
-            💻 Brazilian Front-End developer
-          </h1>
+        <main className="text-white text-center mt-6 w-full">
+          <h1 className="text-xl font-medium">💻 Desenvolvedor Front End</h1>
 
-          <div className="text-sm mt-4 flex flex-col">
-            <p>🚀 Enthusiastic about technology</p>
-            <p>🤓 I love to study and develop efficient solutions</p>
-            <p>🎯 Focused on evolving</p>
+          <span className="block text-sm opacity-60 max-w-xs text-center mx-auto mt-2">
+            Sites, aplicações web, desenvolvimento mobile, landing pages e MAIS!
+          </span>
+
+          <div className="text-md mt-4 flex flex-col">
+            <p>🚀 Entusiasta por tecnlogia</p>
           </div>
 
-          <div className="mt-8 flex flex-col gap-3">
+          <div className="mt-8 flex flex-col gap-3 px-6">
             {links.map((link) => (
               <Link
                 title={link.title}
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,7 +4,7 @@ export function Link({ title, url, description }: LinkProps) {
   return (
     <a
       href={url}
-      className={`flex flex-col text-white bg-emerald-700 rounded-md py-3 hover:bg-emerald-800 transition-colors`}
+      className={`flex flex-col text-white bg-emerald-700 rounded-lg py-3 w-full hover:bg-emerald-800 transition-colors`}
     >
       <span className="">{title}</span>
 
